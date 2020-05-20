58ff2c8bd6c36fb197dadd70bfb52018865d356e
Standa <vajcoj@gmail.com>
2020-05-20 13:55:28 +0200

    API: add swagger

--- CarcassoneAPI/CarcassoneAPI/Startup.cs
+++ CarcassoneAPI/CarcassoneAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System.Net;
 
 namespace CarcassoneAPI
@@ -31,6 +32,13 @@ namespace CarcassoneAPI
             services.AddScoped<IBoardService, BoardService>();
 
             services.AddControllers();
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Carcassone API", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +65,16 @@ namespace CarcassoneAPI
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Carcassone API");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
